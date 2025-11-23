53ef2703aaf554610d9b1d5620c3c6e31190422c
shubhamraj82 <raj.shubh8216@gmail.com>
2025-11-24 05:21:38 +0530

    fix bugs

---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const EmailVerify = () => {
+  return (
+    <div>
+        <h1>
+        Email Verify Page
+    </h1>
+    </div>
+  )
+}
+
+export default EmailVerify
