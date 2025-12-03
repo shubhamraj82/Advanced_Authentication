77aef061630dad5d73517edf809b12794b5a5c9f
shubhamraj82 <raj.shubh8216@gmail.com>
2025-12-04 03:21:34 +0530

    email verification done successfully

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,6 +8,22 @@ import { toast } from 'react-toastify';
 const Navbar = () => {
   const navigate = useNavigate();
   const {userData, backendUrl, setUserData,setIsLoggedIn}=useContext(AppContent);
+
+  const sendVerificationOtp=async()=>{
+    try {
+      axios.defaults.withCredentials=true;
+      const {data}= await axios.post(backendUrl + '/api/auth/send-verify-otp')
+      if(data.success){
+        navigate('/email-verify');
+        toast.success("Verification OTP sent to your email address")
+      }else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  } 
+
   const logout =async()=>{
     try {
       axios.defaults.withCredentials=true
@@ -28,7 +44,7 @@ const Navbar = () => {
           {userData.name.charAt(0).toUpperCase()}
            <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
             <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
-              {!userData.isAccountVerified &&   <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li> 
+              {!userData.isAccountVerified &&   <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li> 
               }
               <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
             </ul>
